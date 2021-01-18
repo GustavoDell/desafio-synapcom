8320d2696efe5552faf992bf62a8ac09d51a0f0a
GustavoDell <gustavo.dell@outlook.com>
2021-01-18 20:40:28 -0300

    finalizando carrossel produto

--- src/components/prateleiraProd/prateleiraProd.jsx
+++ src/components/prateleiraProd/prateleiraProd.jsx
@@ -4,75 +4,80 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./estilo.css"
 
-const produtos = [3808, 3747, 3269, 3808, 1948017390];
+const produtos = [3808, 3747, 3950, 3830, 1948017390];
 
 class PrateleiraProd extends Component{
     constructor(props){
         super(props);
+        this.produto = [];
         this.state = {
-          error: null,
-          isLoaded: false,
-          result: [],
+        
         }
       }
      
-      requisicaoApi(numero){
+      async requisicaoApi(numero){
+            const response = await fetch("/api/catalog_system/pub/products/search/?fq=productId:" + produtos[numero])
+            const data = await response.json();
+            console.log("data:", data[0].items[0].images[0].imageUrl)
+            this.produto.push(data);
+            this.setState({
+                produto: this.produto
+            }) 
+        }
         
-        fetch("/api/catalog_system/pub/products/search/?fq=productId:" + produtos[numero])
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    result: result,
-                });
-                // console.log("fora", result);
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
-      }
-      componentDidMount() {
-        
+
+      componentDidMount(){
         for(var cont = 0; cont <= produtos.length-1; cont++){
-            console.log("cont", cont)
-            console.log("produto", produtos[cont])
             this.requisicaoApi(cont)
         }
-      }
-    render(){
+    }
+    
+        render(){
+       
+        const {produto} = this.produto;
         var settings = {
             dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            fade: true,
-            cssEase: 'linear',
-            width: 500,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4
         };
 
-        const {result} = this.state;
-
         return(
-            <section className="prateleira vitrini">
-                <h2>Você Pode Gostar</h2>
-                <div className="slider-img">
-                    <Slider {...settings}>
-                            {this.state.result.map(item => (
-                                <li>
-                                    <div>
-                                        {console.log("item", item.items.[0].images.[0].imageUrl)}
-                                        <a href={item.items.[0].images.[0].imageUrl}></a>
-                                    </div>
-                                </li>
-                            ))}
-                    </Slider>
+            <section className="content--carouselColection">
+                <div className="content">
+                    <h3>Você Pode Gostar</h3>
+                    <div className="slider-img">
+                        <Slider {...settings}>
+                                {this.produto.map(itens => (
+                                    <li className="slick-product">
+                                        <div className="box-item">
+                                            {console.log("teste", itens)}
+                                            <a className="product-image" href={itens.[0].items.[0].images.[0].imageUrl} tabindex="-1">
+                                                <img src={itens.[0].items.[0].images.[0].imageUrl}></img>
+                                                <span className="imgHover"></span>
+                                            </a>
+                                            <h3 className="product-name">
+                                                <a title={itens.[0].productName} href={itens.[0].link}>
+                                                    {itens.[0].productName}
+                                                </a>
+                                            </h3>
+                                            <div className="box-available">
+                                                <div className="price">
+                                                <a title={itens.[0].productName} href={itens.[0].link}>
+                                                   <div className="prices">
+                                                       <span className="best-price">
+                                                            <strong>R$ {itens.[0].items.[0].sellers.[0].commertialOffer.Price}</strong>
+                                                       </span>
+                                                   </div>
+                                                </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                        </Slider>
+                    </div>
                 </div>
             </section>
         );
